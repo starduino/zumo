1496fb2e03bbc404857eef5593b6b080b82437bb
Ryan <ryan.hartlage@geappliances.com>
2019-07-29 10:56:07 -0400

    Tiny cleanup

--- src/heartbeat.c
+++ src/heartbeat.c
@@ -9,6 +9,7 @@
 #include "stm8.h"
 
 enum {
+  half_period_in_msec = 500,
   pin_5 = (1 << 5)
 };
 
@@ -18,11 +19,11 @@ static void blink(tiny_timer_group_t* group, void* context) {
   (void)group;
   (void)context;
   PB_ODR ^= pin_5;
-  tiny_timer_start(group, &timer, 500, blink, NULL);
+  tiny_timer_start(group, &timer, half_period_in_msec, blink, NULL);
 }
 
 void heartbeat_init(tiny_timer_group_t* timer_group) {
   PB_CR1 |= pin_5;
   PB_DDR |= pin_5;
-  tiny_timer_start(timer_group, &timer, 500, blink, NULL);
+  tiny_timer_start(timer_group, &timer, half_period_in_msec, blink, NULL);
 }
