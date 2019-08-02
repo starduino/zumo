f5c20310764cbe3ba2fc9d186dc7353d9bf10160
Ryan Hartlage <ryanplusplus@gmail.com>
2019-08-01 20:43:28 -0400

    Watchdog

--- src/main.c
+++ src/main.c
@@ -9,15 +9,18 @@
 #include "system_tick.h"
 #include "heartbeat.h"
 #include "tiny_timer.h"
+#include "watchdog.h"
 
 static tiny_timer_group_t timer_group;
 
 void main(void) {
+  watchdog_init();
   clock_init();
   tiny_timer_group_init(&timer_group, system_tick_init());
   heartbeat_init(&timer_group);
 
   while(true) {
     tiny_timer_group_run(&timer_group);
+    watchdog_kick();
   }
 }
