668159de4002d44c272a4d73734de69e578ab391
Ryan Hartlage <ryanplusplus@gmail.com>
2019-08-02 22:10:25 -0400

    Kick watchdog only if system tick interrupt is running

--- src/main.c
+++ src/main.c
@@ -12,6 +12,14 @@
 #include "watchdog.h"
 
 static tiny_timer_group_t timer_group;
+static tiny_timer_t timer;
+
+static void kick_watchdog(tiny_timer_group_t* _timer_group, void* context) {
+  (void)context;
+  (void)_timer_group;
+  watchdog_kick();
+  tiny_timer_start(&timer_group, &timer, 1, kick_watchdog, NULL);
+}
 
 void main(void) {
   watchdog_init();
@@ -19,8 +27,9 @@ void main(void) {
   tiny_timer_group_init(&timer_group, system_tick_init());
   heartbeat_init(&timer_group);
 
+  tiny_timer_start(&timer_group, &timer, 1, kick_watchdog, NULL);
+
   while(true) {
     tiny_timer_group_run(&timer_group);
-    watchdog_kick();
   }
 }
