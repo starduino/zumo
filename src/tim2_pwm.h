98405f41f4abbf8c69dc408109a82b4b41de2aed
Ryan Hartlage <ryanplusplus@gmail.com>
2019-08-09 20:25:21 -0400

    TIM2 PWM

---
+++ src/tim2_pwm.h
@@ -0,0 +1,15 @@
+/*!
+ * @file
+ * @brief
+ */
+
+#ifndef tim2_pwm_h
+#define tim2_pwm_h
+
+#include "i_tiny_pwm.h"
+
+i_tiny_pwm_t* tim2_pwm_channel1_init(void);
+i_tiny_pwm_t* tim2_pwm_channel2_init(void);
+i_tiny_pwm_t* tim2_pwm_channel3_init(void);
+
+#endif
