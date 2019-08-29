c8be351029a5c78eb2dccdcf65638d868ceafc94
Ryan Hartlage <ryanplusplus@gmail.com>
2019-08-28 20:56:05 -0400

    Line sensors

--- src/app/blue-zumo/application.c
+++ src/app/blue-zumo/application.c
@@ -5,20 +5,35 @@
 
 #include "application.h"
 
+#include <stddef.h>
+#include "tiny_utils.h"
+static tiny_event_subscription_t derp_sub;
+
+static void derp_change(void* context, const void* _args) {
+  reinterpret(args, _args, const tiny_key_value_store_on_change_args_t*);
+  (void)context;
+
+  if(args->key == key_right_line_detected) {
+    volatile uint8_t derpity = 10;
+    derpity++;
+  }
+}
+
 static void derp(i_tiny_key_value_store_t* store) {
-  motor_power_t power = 25;
+  motor_power_t power = 0;
   tiny_key_value_store_write(store, key_left_motor, &power);
   tiny_key_value_store_write(store, key_right_motor, &power);
+
+  tiny_event_subscription_init(&derp_sub, NULL, derp_change);
 }
 
 void application_init(application_t* self, tiny_timer_group_t* timer_group) {
-  (void)timer_group;
-
   data_model_init(&self->data_model);
   i_tiny_key_value_store_t* store = data_model_key_value_store(&self->data_model);
 
   motors_plugin_init(&self->motors_plugin, store);
   beep_plugin_init(&self->beep_plugin, store);
+  line_sensors_plugin_init(&self->line_sensors_plugin, store, timer_group);
 
   derp(store);
 }
--- src/app/blue-zumo/data_model.h
+++ src/app/blue-zumo/data_model.h
@@ -7,6 +7,7 @@
 #define data_model_h
 
 #include <stdint.h>
+#include <stdbool.h>
 #include "tiny_ram_key_value_store.h"
 #include "tiny_ram_key_value_store_macros.h"
 #include "motor_power.h"
@@ -14,9 +15,11 @@
 
 // clang-format off
 #define data_model_key_value_pairs(pair) \
-  pair(key_left_motor,    motor_power_t) \
-  pair(key_right_motor,   motor_power_t) \
-  pair(key_beep,          beep_t) \
+  pair(key_left_motor,            motor_power_t) \
+  pair(key_right_motor,           motor_power_t) \
+  pair(key_beep,                  beep_t) \
+  pair(key_left_line_detected,    bool) \
+  pair(key_right_line_detected,   bool) \
 // clang-format on
 
 enumerate_ram_key_value_pairs(data_model_key_value_pairs);
