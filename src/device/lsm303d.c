be85d3751bf814837281d61e93b99bd9be4a79d4
Ryan Hartlage <ryanplusplus@gmail.com>
2019-11-04 06:22:38 -0500

    Add event

--- src/device/lsm303d.c
+++ src/device/lsm303d.c
@@ -17,8 +17,13 @@ enum {
   register_out_x_l_a = 0x28
 };
 
+static void swap_endianness(uint8_t* buffer) {
+  uint8_t temp = buffer[0];
+  buffer[0] = buffer[1];
+  buffer[1] = temp;
+}
+
 static void initialize_accelerometer(lsm303d_t* self);
-static void arm_poll_timer(lsm303d_t* self);
 
 static void read_acceleration_complete(void* context, bool success) {
   reinterpret(self, context, lsm303d_t*);
@@ -27,8 +32,8 @@ static void read_acceleration_complete(void* context, bool success) {
     initialize_accelerometer(self);
   }
   else {
-    // we're in an ISR here, but we want to hand off results and do roughly this:
-    // arm_poll_timer(self);
+    self->busy = false;
+    self->data_ready = true;
   }
 }
 
@@ -42,16 +47,15 @@ static void set_up_acceleration_read_complete(void* context, bool success) {
     tiny_i2c_read(
       self->i2c,
       address,
-      self->raw,
-      sizeof(self->raw),
+      self->read_buffer.raw,
+      sizeof(self->read_buffer),
       read_acceleration_complete,
       self);
   }
 }
 
-static void set_up_acceleration_read(tiny_timer_group_t* group, void* context) {
-  reinterpret(self, context, lsm303d_t*);
-  (void)group;
+static void set_up_acceleration_read(lsm303d_t* self) {
+  self->busy = true;
 
   static const uint8_t buffer[] = {
     register_out_x_l_a | auto_increment
@@ -67,6 +71,36 @@ static void set_up_acceleration_read(tiny_timer_group_t* group, void* context) {
     self);
 }
 
+static void arm_poll_timer(lsm303d_t* self, tiny_timer_group_t* timer_group);
+
+static void poll(tiny_timer_group_t* timer_group, void* context) {
+  reinterpret(self, context, lsm303d_t*);
+
+  if(self->data_ready) {
+    self->data_ready = false;
+
+    swap_endianness((uint8_t*)&self->read_buffer.acceleration.x);
+    swap_endianness((uint8_t*)&self->read_buffer.acceleration.y);
+    swap_endianness((uint8_t*)&self->read_buffer.acceleration.z);
+
+    tiny_single_subscriber_event_publish(
+      &self->acceleration_update,
+      &self->read_buffer.acceleration);
+  }
+
+  if(!self->busy) {
+    set_up_acceleration_read(self);
+  }
+
+  arm_poll_timer(self, timer_group);
+}
+
+static void arm_poll_timer(lsm303d_t* self, tiny_timer_group_t* timer_group) {
+  tiny_timer_start(timer_group, &self->timer, period, poll, self);
+}
+
+static void initialize_accelerometer(lsm303d_t* self);
+
 static void configuration_complete(void* context, bool success) {
   reinterpret(self, context, lsm303d_t*);
 
@@ -74,15 +108,13 @@ static void configuration_complete(void* context, bool success) {
     initialize_accelerometer(self);
   }
   else {
-    arm_poll_timer(self);
+    self->busy = false;
   }
 }
 
-static void arm_poll_timer(lsm303d_t* self) {
-  tiny_timer_start(self->timer_group, &self->timer, period, set_up_acceleration_read, self);
-}
-
 static void initialize_accelerometer(lsm303d_t* self) {
+  self->busy = true;
+
   static const uint8_t buffer[] = {
     register_ctrl1,
     0x57 // 50 Hz, all axes enabled
@@ -100,7 +132,16 @@ static void initialize_accelerometer(lsm303d_t* self) {
 
 void lsm303d_init(lsm303d_t* self, tiny_timer_group_t* timer_group, i_tiny_i2c_t* i2c) {
   self->i2c = i2c;
-  self->timer_group = timer_group;
+  self->busy = false;
+  self->data_ready = false;
+
+  tiny_single_subscriber_event_init(&self->acceleration_update);
 
   initialize_accelerometer(self);
+
+  arm_poll_timer(self, timer_group);
+}
+
+i_tiny_event_t* lsm303d_on_acceleration_update(lsm303d_t* self) {
+  return &self->acceleration_update.interface;
 }
