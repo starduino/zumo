617d810c611d2f820e7246bcb34462f29cf5287d
Robert Curtin <robertecurtin@gmail.com>
2020-03-05 11:43:48 -0500

    Made it actually do things again

--- src/application/tactics/strategist.c
+++ src/application/tactics/strategist.c
@@ -48,40 +48,40 @@ static void data_changed(void* context, const void* _args) {
   reinterpret(args, _args, const tiny_key_value_store_on_change_args_t*);
 
   if(args->key == self->keys->key_enemy_location) {
-    // reinterpret(location, args->value, const enemy_location_t*);
+    reinterpret(location, args->value, const enemy_location_t*);
 
-    // if(!the_current_tactic_is(self, tactic_avoid_line)) {
-    //   if(*location == enemy_location_unknown) {
-    //     choose_seeking_tactic(self);
-    //   }
-    //   else {
-    //     change_tactic_to(self, tactic_charge);
-    //     self->previous_location = *location;
-    //   }
-    // }
+    if(!the_current_tactic_is(self, tactic_avoid_line)) {
+      if(*location == enemy_location_unknown) {
+        choose_seeking_tactic(self);
+      }
+      else {
+        change_tactic_to(self, tactic_charge);
+        self->previous_location = *location;
+      }
+    }
   }
   else if(args->key == self->keys->key_robot_running) {
     choose_initial_tactic(self);
   }
-  // else if(args->key == self->keys->key_line_detected) {
-  //   change_tactic_to(self, tactic_avoid_line);
-  // }
+  else if(args->key == self->keys->key_line_detected) {
+    change_tactic_to(self, tactic_avoid_line);
+  }
   else if(args->key == self->keys->key_tactic_stopped) {
     int8_t power = 0;
-    tiny_key_value_store_write(
-      self->key_value_store,
-      0,
-      &power);
-    tiny_key_value_store_write(
-      self->key_value_store,
-      1,
-      &power);
-    //   if(the_enemy_is_visible(self)) {
-    //     change_tactic_to(self, tactic_charge);
-    //   }
-    //   else {
-    //     choose_seeking_tactic(self);
-    //   }
+    // tiny_key_value_store_write(
+    //   self->key_value_store,
+    //   0,
+    //   &power);
+    // tiny_key_value_store_write(
+    //   self->key_value_store,
+    //   1,
+    //   &power);
+      if(the_enemy_is_visible(self)) {
+        change_tactic_to(self, tactic_charge);
+      }
+      else {
+        choose_seeking_tactic(self);
+      }
   }
 }
 
