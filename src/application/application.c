1c3dc375de7675e5860fa95ab262ec1c0e72993e
Robert Curtin <robertecurtin@gmail.com>
2020-03-06 14:30:48 -0500

    This is the version used for the first competition

--- src/application/application.c
+++ src/application/application.c
@@ -13,10 +13,10 @@ void application_init(application_t* self, tiny_timer_group_t* timer_group) {
 
   motors_plugin_init(&self->motors_plugin, store);
   line_sensors_plugin_init(&self->line_sensors_plugin, store, timer_group);
-  distance_sensors_plugin_init(&self->distance_sensors_plugin, store, timer_group);
+  // distance_sensors_plugin_init(&self->distance_sensors_plugin, store, timer_group);
   accelerometer_plugin_init(&self->accelerometer_plugin, store, timer_group);
   buzzer_plugin_init(&self->buzzer_plugin, store);
   strategy_plugin_init(&self->strategy_plugin, store, timer_group);
   metasensor_plugin_init(&self->metasensor_plugin, store, timer_group);
-  // tactic_feedback_plugin_init(&self->tactic_feedback_plugin, store, timer_group);
+  tactic_feedback_plugin_init(&self->tactic_feedback_plugin, store, timer_group);
 }
