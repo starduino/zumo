cdadabd3a43542afee9d0c8d7951f3f347c82c92
Ryan Hartlage <ryanplusplus@gmail.com>
2022-04-23 14:52:48 -0400

    Updated tools, updated to sdcc 4.2.0

--- target.mk
+++ target.mk
@@ -8,7 +8,7 @@ DEVICE_TYPE := STM8S207
 STLINK := stlinkv2
 OPENOCD_CFG := tools/openocd/stm8s207.cfg
 SVD := tools/svd/stm8s207k8.svd
-TOOLCHAIN_VERSION := 4.0.0
+TOOLCHAIN_VERSION := 4.2.0
 
 include tools/defaults.mk
 
